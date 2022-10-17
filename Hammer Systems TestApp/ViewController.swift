f822a346de675d5ed6a7feb54b578d667ceba77e
rvronski <rvronski@gmail.com>
2022-10-18 00:03:53 +0500

    Initial Commit

---
+++ Hammer Systems TestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hammer Systems TestApp
+//
+//  Created by ROMAN VRONSKY on 18.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
